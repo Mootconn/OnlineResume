f1aee7622ddf1d343f65ab279a28ada7c43be72b
unknown <mootconnor@gmail.com>
2017-12-08 16:12:03 +1300

    Need to fix importing navbar from js

---
+++ www/js/script.js
@@ -0,0 +1,20 @@
+
+document.getElementById("navMenu").innerHTML +=
+  '<div class="row">'+
+    '<div class="col-sm-4">'+
+    ' <!--Spacing-->'+
+    '</div>'+
+    '<div class="col-sm-1"><a href="index.html">HOME</a></div>'+
+    '<div class="col-sm-1">'+
+      '<a href="profile.html">PROFILE</a>'+
+    '</div>'+
+    '<div class="col-sm-1">'+
+      'EXPERIENCE'+
+    '</div>'+
+    '<div class="col-sm-1">'+
+      'CONTACT'+
+    '</div>'+
+    '<div class="col-sm-4">'+
+      '<!--Spacing-->'+
+    '</div>'+
+  '</div>';
